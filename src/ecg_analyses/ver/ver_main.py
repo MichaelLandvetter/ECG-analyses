6d98778a411a15c3859feb78e6371e7469232b77
MichaelLandvetter <michael.axelsson.scott.base@gmail.com>
2026-09-01 15:38:00 +0000

    PR21: normalize ver_main settings import to package path

--- src/ecg_analyses/ver/ver_main.py
+++ src/ecg_analyses/ver/ver_main.py
@@ -75,7 +75,7 @@ from src.ecg_analyses.ver.ver_display import VERDisplayWidget, _FocusableViewBox
 from src.ecg_analyses.ver.ver_filter import BandpassFilter
 from src.ecg_analyses.ver.ver_wavelet import compute_wavelet_scalogram
 from src.ecg_analyses.ver.ver_ml_logger import launch_ml_logger
-from ver_settings import SettingsManager
+from src.ecg_analyses.ver.ver_settings import SettingsManager
 from ver_analysis_flow import (
     BACK_TO_ANALYSIS,
     CANCEL_ANALYSIS,
